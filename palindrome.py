984b093b2b3dbab5d62ae9248f37f5e122cf5fbc
bijjr <bjffrys@gmail.com>
2016-04-01 10:42:13 -0400

    Refactoring

--- palindrome.py
+++ palindrome.py
@@ -1,25 +1,88 @@
-import re
+#Normal Mode
 
-def reverse_string(string):
-    if len(string) == 0:
-        return ''
-    return reverse_string(string[1:]) + string[0]
+import re
+import sys
+
+def reverse(arg):
+    if len(arg) <= 1:
+        return True
+    return arg[0] == arg[-1] and reverse(arg[1:-1])
+
+def regex(arg):
+    pattern = '[^A-Za-z]'
+    string = re.sub(pattern, '', arg.lower())
+    if string[0] != string[-1]:
+        return False
+    return reverse(string)
+
+def is_palindrome(user_input):
+    if len(user_input) <= 1:
+        return True
+    return regex(user_input)
+
+def main():
+    user_input = input("Enter a string> ")
+
+if __name__ == "__main__":
+    main()
+
+
+#Advanced
+
+def regex(arg):
+    pattern = '[^A-Za-z]'
+    string = re.sub(pattern, '', arg.lower())
 
 def is_palindrome(user_input):
     pattern = '[^A-Za-z]'
-    string = re.sub(pattern, '', user_input.lower())
-    print("This is the lower " + string)
-    backward_input = reverse_string(string)
-    print(backward_input + " is the backwards string")
-    print(string + " is the regular string")
-    if backward_input == string:
+    old_string = re.sub(pattern, '', user_input.lower())
+    new_string =''
+    for i in old_string[::-1]:
+        new_string += i
+    if new_string == old_string:
+        print("{} is a palindrome".format(user_input))
         return True
     else:
+        print("{} is not a palindrome".format(user_input))
         return False
 
 def main():
-    user_input = input("Enter a word/sentence > ")
-    is_palindrome(user_input)
+    #try to add regex here??
+    user_input = input("Enter a string? ")
+    return is_palindrome(user_input)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    main()
+
+#Epic Mode
+def regex(arg):
+    pattern = '[^A-Za-z]'
+    new_line = re.sub(pattern, '', arg.lower())
+    print('cleaned     ' + new_line)
+    return reverse(new_line)
+
+def reverse(arg):
+    if len(arg) <= 1:
+        return True
+    return arg[0] == arg[-1] and reverse(arg[1:-1])
+
+def is_palindrome(arg):
+    if len(arg) <= 1:
+        print("{} is a palindrome".format(arg))
+        return True
+    elif regex(arg):
+        print("{} is a palindrome".format(arg))
+        return True
+    else:
+        print("{} is not a palindrome".format(arg))
+        return False
+
+def main():
+    with open(sys.argv[1], 'r') as file:
+        for line in file:
+            #line = old_line
+            print(line,end='  ')
+            is_palindrome(line)
+
+if __name__ == '__main__':
     main()
